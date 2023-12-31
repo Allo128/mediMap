7961181ac95279bd938cce1aae48fe2831605719
Allo128 <153752866+Allo128@users.noreply.github.com>
2023-12-31 23:06:57 +0900

    Update project.js

--- project.js
+++ project.js
@@ -1,12 +1,15 @@
-import Map from "ol/Map.js";
-import View from "ol/View.js";
-import OSM from "ol/source/OSM.js";
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
 
-const map = new Map({target: "map"});
-map.setView(new View({
-  center: [0,0];
-  zoom: 2;
-}));
-const source = OSM();
-const layer = new TileLayer({source: source});
-map.addLayer(layer);
+new Map({
+  layers: [
+    new TileLayer({source: new OSM()}),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+  target: 'map',
+});
