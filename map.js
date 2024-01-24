f38554403c8b30a0eaac83cf1f21c81fc17f8d51
Allo128 <153752866+Allo128@users.noreply.github.com>
2024-01-24 17:40:48 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,16 +1,37 @@
-function init_map() {
-  const map = new ol.Map({
-    target: 'container',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([131.5,33.2]), 
-      zoom: 6 
+//function init_map() {
+
+
+
+const map = new ol.Map({
+  target: 'container',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
     })
-  });
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([36,140]), 
+    zoom: 6,
+    maxZoom : 5,
+    minZoom : 20
+  })
+});
+
+const geolocation = new ol.Geolocation({
+  trackingOptions: {
+    enableHighAccuracy: true,
+  },
+  projection: ol.View.getProjection(),
+});
+
+function el(id) {
+  return document.getElementById(id);
 }
+
+
+//}
+
+
+
 
 window.addEventListener('DOMContentLoaded', init_map());
