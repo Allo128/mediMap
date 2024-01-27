7970ee249169d3e6078b0da3900ede72ea075f5a
Allo128 <153752866+Allo128@users.noreply.github.com>
2024-01-27 11:25:32 +0900

    Update map.js

--- map.js
+++ map.js
@@ -2,7 +2,7 @@ const mapview = new ol.View({
   projection: "EPSG:4326",
   center: [131.407244, 33.300000],
   //center: ol.proj.fromLonLat([131.407244, 33.182399]),
-  zoom: 2,
+  zoom: 10,
   //extent: [129.534766, 33.934488, 132.014351, 31.153983],
   //extent: ol.proj.transformExtent([130.875630, 33.665779, 132.102984, 32.735597], "EPSG:4326", "EPSG:3857"),
   /*
