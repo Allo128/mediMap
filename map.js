1ef6ec38f23d1ba99cbc8685c24cd8e6908d4460
Allo128 <153752866+Allo128@users.noreply.github.com>
2024-01-30 21:00:53 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,16 +1,10 @@
 const mapview = new ol.View({
-  //projection: "EPSG:4326",
   //center: [131.463774, 33.227400],
   center: ol.proj.fromLonLat([131.463774, 33.227400]),
   minZoom : 7,
   zoom: 7,
   //extent: [129.534766, 33.934488, 132.014351, 31.153983],  
   extent: ol.proj.transformExtent([130.824563, 32.714204, 132.102984, 33.740596], "EPSG:4326", "EPSG:3857"),
-  /*
-  center: fromLonLat([139.767, 35.681]),
-  zoom: 11,
-  extent: transformExtent([139.7568, 35.6746, 139.7774, 35.6877], 'EPSG:4326', 'EPSG:3857')
-  */
 });
 
 
@@ -30,26 +24,18 @@ const map = new ol.Map({
   }),
   */
   
-  //controls: ol.control.defaults().extend([new ol.control.ZoomSlider()]),
+  controls: ol.control.defaults().extend([new ol.control.ZoomSlider()]),
 
 });
 
 
 
-/*
-setTimeout(map.on("postcompose",updateView),500);
-
-function updateView() {
-  mapview.setCenter(geolocation.getPosition());
-}
-*/
 
 const geolocation = new ol.Geolocation({
   trackingOptions: {
     enableHighAccuracy: true
   },
   projection: "EPSG:4326",
-    //mapview.getProjection(),
 });
 
 function el(id) {
@@ -57,20 +43,17 @@ function el(id) {
 }
 
 el("track").addEventListener("change", function(){
-  //ol.View.setCenter([geolocation.getPosition()]);
   //geolocation.setTracking(this.checked);
   const coord = geolocation.getPosition();
   mapview.setCenter(ol.proj.transform(coord,"EPSG:4326","EPSG:3857"));
   //alert(Array.isArray(geolocation.getPosition()));
   //alert(geolocation.getPosition());
-  //ol.View.setCenter(geolocation.getPosition());
 });
 
 geolocation.on("change", function(){
   el('accuracy').innerText = geolocation.getAccuracy() + ' [m]';
   el('position').innerText = geolocation.getPosition() + ' [m]';
   el('speed').innerText = geolocation.getSpeed() + ' [m/s]';
-  //mapview.centerOn(geolocation.getPosition(), map.getSize(), [570,500]);
 });
 
 geolocation.on("error",function(){
@@ -128,9 +111,6 @@ function currentSet(){
 window.addEventListener("DOMContentLoaded",function(){
   setTimeout(function(){
     geolocation.setTracking(true);
-    mapview.setCenter(ol.proj.transform(geolocation.getPosition(),"EPSG:4326","EPSG:3857"));
-  },100)
-  //positionFeature.setGeometry(geolocation.getPosition() ? new ol.geom.Point(coordinates) : null);
-  //map.render();
-  //ol.View.setCenter(geolocation.getPosition());
+    //mapview.setCenter(ol.proj.transform(geolocation.getPosition(),"EPSG:4326","EPSG:3857"));
+  },100);
 });
