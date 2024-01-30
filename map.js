6b8db9afd1fa054e33783cb05dab14df567de6af
Allo128 <153752866+Allo128@users.noreply.github.com>
2024-01-31 02:32:24 +0900

    Update map.js

--- map.js
+++ map.js
@@ -114,6 +114,17 @@ function setDestination(lng,lat,name){
   );
 }
 
+  map.on(['click'], function(evt) {
+    let feature = map.forEachFeatureAtPixel(evt.pixel,function(feature) {return feature;});
+    if (feature) {
+      content.innerHTML = feature.get('name');
+      var coordinate = feature.getGeometry().getCoordinates();
+      popoverlay.setPosition(coordinate);
+      map.getViewport().style.cursor = 'pointer';
+    }
+  });
+
+
 
 
 geolocation.on('change:position', function(){
