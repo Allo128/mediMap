58cbc4db1209885f13b38ed2f880f56eaaec259f
Allo128 <153752866+Allo128@users.noreply.github.com>
2024-02-01 23:21:06 +0900

    Update map.js

--- map.js
+++ map.js
@@ -308,8 +308,12 @@ function setHospital(){
   }
   
 }
+
+
 function coder(address){
   const id = "dj00aiZpPVV6WlpKTm0xWndUZiZzPWNvbnN1bWVyc2VjcmV0Jng9MmI-";
+  const encoded = encodeURI(address);
+  const url = `https://map.yahooapis.jp/geocode/V1/geoCoder?appid=${id}&query=${encoded}`;
 }
 
 
